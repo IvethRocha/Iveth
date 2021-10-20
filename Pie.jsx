6d1e12d1e670fc39063360d30abd3def4818bd7b
IvethRocha <87579051+IvethRocha@users.noreply.github.com>
2021-10-20 02:03:24 -0500

    Create Pie.jsx

---
+++ Pie.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Pie = () => (
+  <footer id="pie">
+    <p>DERECHOS RESERVADOS © 2021. DISEÑADO POR IVETH ROCHA.</p>
+    <ul>
+                <li>Blog</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Privacy Policy</li>
+            </ul>
+  </footer>
+)
+
+export default Pie;
